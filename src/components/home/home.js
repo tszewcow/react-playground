6d799c9466853279e1425d5910375a68e9118d0e
Tomek Szewcow <tomek.szewcow@gmail.com>
2020-02-28 00:31:30 +0100

    carousel for home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,16 +1,55 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import {Jumbotron, Button} from 'react-bootstrap';
+import { Jumbotron, Button, Carousel } from 'react-bootstrap';
 
 export const Home = () => (
-    <Jumbotron>
-        <h1>Sample react app</h1>
-        <p>
-            This is a simple react app build on top of react-bootstrap library.
+    <>
+        <Jumbotron>
+            <h1>Sample react app</h1>
+            <p>
+                This is a simple react app build on top of react-bootstrap library.
         </p>
-        <p>
-            <Button as={Link} to="/about" variant="primary">Learn more</Button>
-        </p>
-    </Jumbotron>
+            <p>
+                <Button as={Link} to="/about" variant="primary">Learn more</Button>
+            </p>
+        </Jumbotron>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://images.unsplash.com/photo-1535209252068-f498eac4aaa3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://images.unsplash.com/photo-1581244102548-ccd86c3f9d2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://images.unsplash.com/photo-1581520296765-b84e8bcee527?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    </>
 )
