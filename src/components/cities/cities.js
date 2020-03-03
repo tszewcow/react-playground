2ab1484ac4a141f220404d4023b055fd8b58fb5a
Tomek Szewcow <tomek.szewcow@gmail.com>
2020-03-03 20:20:20 +0100

    cities as functional component

--- src/components/cities/cities.js
+++ src/components/cities/cities.js
@@ -1,45 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { CitiesTable } from '../cities-table/cities-table';
 import { LoadingSpinner } from '../loading-spinner/loading-spinner';
 import styles from './cities.module.scss';
 
-export class Cities extends Component {
+export const Cities = () => {
 
-    constructor(props) {
-        super(props);
+    const [cities, setCities] = useState([]);
 
-        this.state = {
-            cities: []
-        }
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         import('../../data/cities.json').then(data => {
-            setTimeout(() => this.setState({ cities: data.default }), 2000);
+            setTimeout(() => setCities(data.default), 2000);
         });
-    }
+    }, [])
 
-    onDetailsClick = (event) => {
+    const onDetailsClick = (event) => {
         const cityId = event.target.getAttribute('city');
-        console.log(this.state.cities.find(city => city.id === cityId));
+        console.log(cities.find(city => city.id === cityId));
     }
 
-    onDeleteClick = (event) => {
+    const onDeleteClick = (event) => {
         const cityId = event.target.getAttribute('city');
-        const index = this.state.cities.findIndex(city => city.id === cityId);
-        this.setState(prev => ({
-            cities: prev.cities.slice(0, index).concat(prev.cities.slice(index + 1))
-        }))
+        const index = cities.findIndex(city => city.id === cityId);
+        setCities(cities.slice(0, index).concat(cities.slice(index + 1)));
     }
 
-    render() {
-        return (
-            <div className={styles.cities_container}>
-                {this.state.cities.length ?
-                    <CitiesTable cities={this.state.cities} onDetailsClick={this.onDetailsClick} onDeleteClick={this.onDeleteClick} /> :
-                    <LoadingSpinner />}
-            </div>
-        )
-    }
+    return (
+        <div className={styles.cities_container}>
+            {cities.length ?
+                <CitiesTable cities={cities} onDetailsClick={onDetailsClick} onDeleteClick={onDeleteClick} /> :
+                <LoadingSpinner />}
+        </div>
+    )
 }
