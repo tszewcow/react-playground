a50c20ecb7e5a708d12b6b0648ed83b8fc084acd
Tomek Szewcow <tomek.szewcow@gmail.com>
2020-03-04 20:28:00 +0100

    city details with loading spinner

--- src/components/city-details/city-details.js
+++ src/components/city-details/city-details.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import {Link} from 'react-router-dom';
-import {Button} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
-export const CityDetails = () => (
-    <div>
-        <Button as={Link} variant="primary" to='/cities'>Go back</Button>
-    </div>
-)
+import { LoadingSpinner } from '../loading-spinner/loading-spinner';
+
+export const CityDetails = props => {
+
+    const [city, setCity] = useState({});
+    const [dataLoaded, setDataLoaded] = useState(false);
+
+    useEffect(() => {
+        import('../../data/cities.json').then(data => {
+            setTimeout(() => {
+                setCity(data.default.find(city => city.id === props.match.params.id));
+                setDataLoaded(true);
+            }, 2000);
+        });
+    }, [])
+
+    return (
+        <div>
+            {dataLoaded ? <><Button as={Link} variant="primary" to='/cities'>Go back</Button><h2>{city.name}</h2></> : <LoadingSpinner />}
+        </div>
+    )
+}
